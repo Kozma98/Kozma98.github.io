3b3cba4454cb7e713e955385e717bcfb96d35efb
Kozma98 <calebkozma@gmail.com>
2018-08-20 01:01:42 -0500

    Fixing error part 1

--- js/custom.js
+++ js/custom.js
@@ -8,15 +8,17 @@ $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
 
    // animate
    $('html, body').animate({
-       scrollTop: $(hash).offset().top -50
-     }, 700, function(){
+       scrollTop: $(hash).offset().top
+     }, 500, function(){
 
        // when done, add hash to url
        // (default click behaviour)
        window.location.hash = hash;
      });
-    
-$("a.navbar-brand[href^='#']").on('click', function(e) {
+
+});
+
+$("a.navbar-brand[href^='#jumbotron']").on('click', function(e) {
 
    // prevent default anchor click behavior
    e.preventDefault();
@@ -26,10 +28,13 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
 
    // animate
    $('html, body').animate({
-       scrollTop: $(hash).offset().top -50
-     }, 700, function(){
+       scrollTop: $(hash).offset().top
+       scrollTop: $(hash).offset().top
+     }, 500, function(){
 
        // when done, add hash to url
        // (default click behaviour)
        window.location.hash = hash;
      });
+
+});
